06436625c51df80f44dffdd4be84d0946bd00bb9
Pablo Paau Cho <pfpaau@gmail.com>
2022-05-05 13:33:17 -0600

    cambio de color

--- CapaPresentacion/frm_verificarTraslados.Designer.cs
+++ CapaPresentacion/frm_verificarTraslados.Designer.cs
@@ -63,10 +63,10 @@ namespace CapaPresentacion
             this.pnl_parametros.Controls.Add(this.cmb_tipoPoliza);
             this.pnl_parametros.Controls.Add(this.lbl_fechaU);
             this.pnl_parametros.Controls.Add(this.cmb_empresa);
-            this.pnl_parametros.Location = new System.Drawing.Point(13, 13);
-            this.pnl_parametros.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_parametros.Location = new System.Drawing.Point(20, 20);
+            this.pnl_parametros.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pnl_parametros.Name = "pnl_parametros";
-            this.pnl_parametros.Size = new System.Drawing.Size(937, 202);
+            this.pnl_parametros.Size = new System.Drawing.Size(1406, 311);
             this.pnl_parametros.TabIndex = 20;
             this.pnl_parametros.Tag = "";
             // 
@@ -78,10 +78,10 @@ namespace CapaPresentacion
             "01",
             "02",
             "03"});
-            this.cmb_caja.Location = new System.Drawing.Point(692, 87);
-            this.cmb_caja.Margin = new System.Windows.Forms.Padding(4);
+            this.cmb_caja.Location = new System.Drawing.Point(1038, 134);
+            this.cmb_caja.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cmb_caja.Name = "cmb_caja";
-            this.cmb_caja.Size = new System.Drawing.Size(98, 24);
+            this.cmb_caja.Size = new System.Drawing.Size(145, 29);
             this.cmb_caja.TabIndex = 5;
             this.cmb_caja.Text = "Seleccione";
             // 
@@ -89,10 +89,10 @@ namespace CapaPresentacion
             // 
             this.btn_verificar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_verificar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_verificar.Location = new System.Drawing.Point(57, 141);
-            this.btn_verificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_verificar.Location = new System.Drawing.Point(86, 217);
+            this.btn_verificar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btn_verificar.Name = "btn_verificar";
-            this.btn_verificar.Size = new System.Drawing.Size(164, 36);
+            this.btn_verificar.Size = new System.Drawing.Size(246, 55);
             this.btn_verificar.TabIndex = 6;
             this.btn_verificar.Text = "Verificar";
             this.btn_verificar.UseVisualStyleBackColor = true;
@@ -100,26 +100,28 @@ namespace CapaPresentacion
             // 
             // txt_anio
             // 
-            this.txt_anio.Location = new System.Drawing.Point(258, 89);
+            this.txt_anio.Location = new System.Drawing.Point(387, 137);
+            this.txt_anio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_anio.Name = "txt_anio";
-            this.txt_anio.Size = new System.Drawing.Size(98, 20);
+            this.txt_anio.Size = new System.Drawing.Size(145, 26);
             this.txt_anio.TabIndex = 2;
             // 
             // txt_mes
             // 
-            this.txt_mes.Location = new System.Drawing.Point(399, 89);
+            this.txt_mes.Location = new System.Drawing.Point(598, 137);
+            this.txt_mes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txt_mes.Name = "txt_mes";
-            this.txt_mes.Size = new System.Drawing.Size(98, 20);
+            this.txt_mes.Size = new System.Drawing.Size(145, 26);
             this.txt_mes.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(540, 61);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(810, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.Size = new System.Drawing.Size(123, 19);
             this.label1.TabIndex = 22;
             this.label1.Text = "Tipo de poliza";
             // 
@@ -127,10 +129,10 @@ namespace CapaPresentacion
             // 
             this.lbl_caja.AutoSize = true;
             this.lbl_caja.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_caja.Location = new System.Drawing.Point(692, 61);
-            this.lbl_caja.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_caja.Location = new System.Drawing.Point(1038, 94);
+            this.lbl_caja.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_caja.Name = "lbl_caja";
-            this.lbl_caja.Size = new System.Drawing.Size(84, 15);
+            this.lbl_caja.Size = new System.Drawing.Size(128, 19);
             this.lbl_caja.TabIndex = 21;
             this.lbl_caja.Text = "Caja Contable";
             // 
@@ -138,10 +140,10 @@ namespace CapaPresentacion
             // 
             this.lbl_parametros.AutoSize = true;
             this.lbl_parametros.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_parametros.Location = new System.Drawing.Point(15, 14);
-            this.lbl_parametros.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_parametros.Location = new System.Drawing.Point(22, 22);
+            this.lbl_parametros.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_parametros.Name = "lbl_parametros";
-            this.lbl_parametros.Size = new System.Drawing.Size(263, 16);
+            this.lbl_parametros.Size = new System.Drawing.Size(386, 23);
             this.lbl_parametros.TabIndex = 17;
             this.lbl_parametros.Text = "Datos para verificar traslado de ventas";
             // 
@@ -149,10 +151,11 @@ namespace CapaPresentacion
             // 
             this.lbl_empresa.AutoSize = true;
             this.lbl_empresa.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_empresa.Location = new System.Drawing.Point(99, 61);
-            this.lbl_empresa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_empresa.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbl_empresa.Location = new System.Drawing.Point(148, 94);
+            this.lbl_empresa.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_empresa.Name = "lbl_empresa";
-            this.lbl_empresa.Size = new System.Drawing.Size(104, 15);
+            this.lbl_empresa.Size = new System.Drawing.Size(160, 19);
             this.lbl_empresa.TabIndex = 2;
             this.lbl_empresa.Text = "Empresa Contable";
             // 
@@ -160,10 +163,10 @@ namespace CapaPresentacion
             // 
             this.lbl_fechaI.AutoSize = true;
             this.lbl_fechaI.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_fechaI.Location = new System.Drawing.Point(258, 61);
-            this.lbl_fechaI.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_fechaI.Location = new System.Drawing.Point(387, 94);
+            this.lbl_fechaI.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_fechaI.Name = "lbl_fechaI";
-            this.lbl_fechaI.Size = new System.Drawing.Size(63, 15);
+            this.lbl_fechaI.Size = new System.Drawing.Size(94, 19);
             this.lbl_fechaI.TabIndex = 4;
             this.lbl_fechaI.Text = "Año Fiscal";
             // 
@@ -173,10 +176,10 @@ namespace CapaPresentacion
             this.cmb_tipoPoliza.FormattingEnabled = true;
             this.cmb_tipoPoliza.Items.AddRange(new object[] {
             "PDV"});
-            this.cmb_tipoPoliza.Location = new System.Drawing.Point(540, 87);
-            this.cmb_tipoPoliza.Margin = new System.Windows.Forms.Padding(4);
+            this.cmb_tipoPoliza.Location = new System.Drawing.Point(810, 134);
+            this.cmb_tipoPoliza.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cmb_tipoPoliza.Name = "cmb_tipoPoliza";
-            this.cmb_tipoPoliza.Size = new System.Drawing.Size(109, 24);
+            this.cmb_tipoPoliza.Size = new System.Drawing.Size(162, 29);
             this.cmb_tipoPoliza.TabIndex = 4;
             this.cmb_tipoPoliza.Text = "Seleccione";
             // 
@@ -184,10 +187,10 @@ namespace CapaPresentacion
             // 
             this.lbl_fechaU.AutoSize = true;
             this.lbl_fechaU.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_fechaU.Location = new System.Drawing.Point(399, 61);
-            this.lbl_fechaU.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_fechaU.Location = new System.Drawing.Point(598, 94);
+            this.lbl_fechaU.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbl_fechaU.Name = "lbl_fechaU";
-            this.lbl_fechaU.Size = new System.Drawing.Size(63, 15);
+            this.lbl_fechaU.Size = new System.Drawing.Size(94, 19);
             this.lbl_fechaU.TabIndex = 6;
             this.lbl_fechaU.Text = "Mes Fiscal";
             // 
@@ -198,10 +201,10 @@ namespace CapaPresentacion
             this.cmb_empresa.Items.AddRange(new object[] {
             "1",
             "99"});
-            this.cmb_empresa.Location = new System.Drawing.Point(99, 87);
-            this.cmb_empresa.Margin = new System.Windows.Forms.Padding(4);
+            this.cmb_empresa.Location = new System.Drawing.Point(148, 134);
+            this.cmb_empresa.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.cmb_empresa.Name = "cmb_empresa";
-            this.cmb_empresa.Size = new System.Drawing.Size(116, 24);
+            this.cmb_empresa.Size = new System.Drawing.Size(172, 29);
             this.cmb_empresa.TabIndex = 1;
             this.cmb_empresa.Text = "Seleccione";
             // 
@@ -213,21 +216,24 @@ namespace CapaPresentacion
             this.dgv_verificarTraslados.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dgv_verificarTraslados.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgv_verificarTraslados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_verificarTraslados.Location = new System.Drawing.Point(13, 223);
+            this.dgv_verificarTraslados.Location = new System.Drawing.Point(20, 343);
+            this.dgv_verificarTraslados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgv_verificarTraslados.Name = "dgv_verificarTraslados";
             this.dgv_verificarTraslados.ReadOnly = true;
-            this.dgv_verificarTraslados.Size = new System.Drawing.Size(937, 249);
+            this.dgv_verificarTraslados.RowHeadersWidth = 62;
+            this.dgv_verificarTraslados.Size = new System.Drawing.Size(1406, 383);
             this.dgv_verificarTraslados.TabIndex = 21;
             // 
             // frm_verificarTraslados
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(977, 499);
+            this.ClientSize = new System.Drawing.Size(1466, 768);
             this.Controls.Add(this.dgv_verificarTraslados);
             this.Controls.Add(this.pnl_parametros);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frm_verificarTraslados";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Verificacion de traslados";
